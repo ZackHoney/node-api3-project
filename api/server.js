300adcc77f0d7fbff83a2ae5abc0d6f255f8b504
Zack Honey <zhoney466@gmail.com>
2023-10-04 10:40:29 -0700

    Got logger set up

--- api/server.js
+++ api/server.js
@@ -1,24 +1,19 @@
 const express = require('express');
-const userRouter = require('./users/users-router');
-
+const { logger } = require('./middleware/middleware')
 const server = express();
 
-
-// remember express by default cannot parse JSON in request bodies
 server.use(express.json());
+
+server.use(logger)
 // global middlewares and the user's router need to be connected here
 
-// server.use('/api/users', userRouter)
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-server.use('*', (req, res) => {
-  res.status(404).json({
-    message: `${req.method} ${req.baseUrl} not found!`
-  })
-})
+
 
 
 
